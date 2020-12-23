2693a2a7c14abaad2b6e2e74603f355c6b0b789f
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2020-12-23 18:57:29 +0300

    Add files via upload

--- vue/component_to_do_list.js
+++ vue/component_to_do_list.js
@@ -1,21 +1,43 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">Счётчик кликов — {{ count }}</button>'
+})
+
+
+
 Vue.component('todo-item', {
   props: ['todo'], // income element
-  template:  '<li >{{ todo.text }}</li>'
+  template:  '<p>{{ todo.text }}</p><span></span>'
 })
   
 var app = new Vue({
   el: '#app',
    data: {
+   	 visible: false,
+   	 date_now:null,
+   	 
+   	//lists of data 
     groceryList: [
-      { id: 0, text: 'Овощи' },
-      { id: 1, text: 'Сыр' },
-      { id: 2, text: 'Что там ещё люди едят?' }
+      { id: 0, text: 'Programming' },
+      { id: 1, text: 'Observations' },
+      { id: 2, text: 'Documentation' }
     ],
     photoList:[
      {id:0,link:"ph.png",text:"Python"},
      {id:1,link:"js.png", text:"JavaScript"},
      {id:2,link:"java.png",text:"Java"}
     ]
+
+    
+  },
+  methods:{
+  	tryme(){
+  		this.date_now = Date()
+  	}
   }
-  
+ 
 })
