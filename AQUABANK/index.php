0fccbf92e448245c15ded9280d3f8476379d6193
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-03-19 20:31:06 +0300

    Add files via upload

--- AQUABANK/index.php
+++ AQUABANK/index.php
@@ -1,12 +1,18 @@
 <html>
 <head>
     <title>AQUA BANK</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <center>
 <br>
 <h1>Aqua Bank</h1>
-
+<div class="main"> 
+    <div id="panel">
+    <a href="index.php">Main</a>
+    <a href="index.php">Get card</a>
+    </div>
+</div>
 </center>
 
 <script  text="javascript">
